34f83abe5063d5e05b237cc8bb391e6b9337ef94
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-07-27 12:19:13 -0600

    classes defined

--- server/models.py
+++ server/models.py
@@ -11,13 +11,25 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+
+    animals = db.relationship('Animal', back_populates='zookeepers')
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+    
+    animals = db.relationship('Animal', back_populates='enclosures')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    enclosure = db.relationship('Enclosure', back_populates='animals')
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
